f82e65fc06d97cdcff78176ea026f73e71743a91
adiwon9555 <aditya.rathi9555@gmail.com>
2019-03-01 12:58:11 +0530

    for zylotech

---
+++ precache-manifest.d73f5d41146486276e149d67dd88d3e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "16675ccd314999748958",
+    "url": "/static/js/main.16675ccd.chunk.js"
+  },
+  {
+    "revision": "1ab86e178e934daf8a63",
+    "url": "/static/js/2.1ab86e17.chunk.js"
+  },
+  {
+    "revision": "16675ccd314999748958",
+    "url": "/static/css/main.3a3cb3d0.chunk.css"
+  },
+  {
+    "revision": "1ab86e178e934daf8a63",
+    "url": "/static/css/2.9ba9d28d.chunk.css"
+  },
+  {
+    "revision": "ce5a7fb99a760c500a9613b8b7c202e9",
+    "url": "/index.html"
+  }
+];
